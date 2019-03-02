5159f37fc0c1b1f4840ac66cdc2bf3e2b6c4d579
A01205336 <47878118+A01205336@users.noreply.github.com>
2019-03-02 15:19:52 -0600

    Add files via upload

--- A_star.py
+++ A_star.py
@@ -88,12 +88,10 @@ if __name__ =="__main__":
     min_heap = []
 
 
-    lines.append("2" )
-    lines.append("(A); (B); (C)")
-    lines.append("(A, C); X; X")
 
-    #for line in fileinput.input():
-     #   lines.append(line)
+
+    for line in fileinput.input():
+        lines.append(line)
 
     height=int(lines[0])
     ini=lines[1]
